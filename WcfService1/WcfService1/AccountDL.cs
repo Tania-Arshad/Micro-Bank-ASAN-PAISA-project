e36af4b61ad5963caf79b36beb341fc27f9ce7f5
Aneesa-Amjad <aneesaamjad2207@gmail.com>
2018-04-26 01:47:30 +0500

    i deal with AccountDL class

---
+++ WcfService1/WcfService1/AccountDL.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WcfService1
+{
+    public class AccountDL
+    {
+        public static List<Account> Accounts = new List<Account>();
+    }
+}
